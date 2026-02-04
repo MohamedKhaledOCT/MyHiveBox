303a423f023da2e89d0e56ba92253dd5b7f34013
mohamed khaled <mohamedkhaledworkmail@gmail.com>
2026-02-04 20:10:17 +0200

    pylint fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,19 @@
+from unittest.mock import patch, MagicMock
 import pytest
 import requests_mock
-from unittest.mock import patch, MagicMock
 from app import app
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
-    
-    # 1. نجهز رديس "وهمي" (Fake Redis)
+
+    # 1. Mock Redis
     mock_redis = MagicMock()
-    mock_redis.get.return_value = None  # الكاش فارغ
-    mock_redis.exists.return_value = 0  # المفتاح غير موجود
-    mock_redis.ping.return_value = True # الاتصال سليم
-    
-    # 2. نستبدل الرديس الحقيقي بالوهمي أثناء الاختبار
+    mock_redis.get.return_value = None
+    mock_redis.exists.return_value = 0
+    mock_redis.ping.return_value = True
+
+    # 2. Apply Patch
     with patch('app.CACHE', mock_redis):
         with app.test_client() as client:
             yield client
@@ -33,7 +33,7 @@ def test_temperature_endpoint(client):
                 "createdAt": "2024-01-01T12:00:00.000Z"
             }}]
         })
-        
+
         response = client.get('/temperature')
         assert response.status_code == 200
         data = response.json
